2ac937bb0a1d81f1d5b55d3258cb17aea0acf9b4
saboorqais <saboor.qaiser456@gmail.com>
2023-08-08 06:44:52 -0700

    Connected Postgres to GraphQL

--- Utils/resolvers.js
+++ Utils/resolvers.js
@@ -1,22 +1,15 @@
 // resolvers.js
 
-const employees = [
-    {
-      id: "1",
-      name: 'John Doe',
-      job: 'Software Engineer',
-      department: 'Engineering',
-      salary: 80000,
-      hire_date: '2022-01-15',
-    },
-    // ...other employee objects
-  ];
   
   const resolvers = {
     Query: {
       getEmployee: (parent, {id}) => employees.find(employee => employee.id === id),
-      listEmployees: () => employees,
-    },
+      listEmployees: async (parent, args, context) => {
+        console.log(context)
+        // Use the PostgreSQL query here to fetch the list of employees
+        const result = await context.client.query('SELECT * FROM employee');
+        return result.rows;
+    }}
   };
   
   module.exports = resolvers;
--- app.js
+++ app.js
@@ -7,27 +7,19 @@ const { ApolloServer } = require("@apollo/server")
 const { expressMiddleware } = require("@apollo/server/express4")
 const resolvers = require("./Utils/resolvers")
 const typeDefs = require("./models/employee")
-const { Client } = require("pg")
+const client = require("./DatabaseClient/Client")
 
 
 
 
 async function StartServer() {
     const app = express();
-    const server = new ApolloServer({ typeDefs: typeDefs, resolvers: resolvers })
+    
     const userRoutes = require('./routes/user');
 
-    const client = new Client({
-        user: "postgres",
-        host: "localhost",
-        database: "Organization",
-        password: "admin",
-        port: 5432
-    }) 
     client.connect().then((res) => { console.log("Connected to Database") }).catch(err => console.log(err))
  
-    client.query("select * from Employee").then(res=>{console.log(res.rows)}).catch(err=>{console.log(err)})
-
+    const server = new ApolloServer({ typeDefs: typeDefs, resolvers: resolvers})
     // Middleware
     app.use(morgan('dev'));
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,7 +31,11 @@ async function StartServer() {
 
     // routes
     app.use("/user", userRoutes);
-    app.use("/graphql", expressMiddleware(server))
+    app.use("/graphql", expressMiddleware(server,{
+        context: ({ req }) => {
+            console.log('DB client'); 
+            return { client:client }   }
+    }))
 
 
     const port = 3001
